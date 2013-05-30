53745e708390e5f5007ce65eccc4f4b9b4d237b2
fox2 <fox@fox-vm2>
2013-05-30 09:59:40 +0800

    multiprocessing: 1. pool 2. use of join 3. show process id and parent process id (Unix)

---
+++ PythonPractice/multiprocessing/003-process-02.py
@@ -0,0 +1,31 @@
+'''
+Created on May 30, 2013
+
+@author: fox
+'''
+
+from multiprocessing import Process    
+import os
+
+
+def info(title):
+    print(title)
+    print('module name: ', __name__)
+    
+    if hasattr(os,'getppid'):
+        print('Parent Process: ', os.getppid())
+    print('process ID: ', os.getpid())
+
+def f (name):
+    info('function f')
+    print('Hello', name)
+    
+def main():
+    info('main line')
+    p = Process(target = f,args=('Fox',))
+    p.start()
+    p.join()
+
+if __name__ == '__main__':main()
+    
+    
